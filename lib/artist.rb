f0680ead3bd2a0d560132f1eddd49ad0c1118d8d
Christina Williams <christinaxtwilliams@gmail.com>
2018-10-12 18:58:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,5 +7,7 @@ class MP3Importer
 def files
     @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
   end
-  
+  def import
+    files.each {|song| Song.new_by_filename(song)}
+  end
   
