eb9d54e57981a97b1fa98738aec4622adc6368fe
Christina Williams <christinaxtwilliams@gmail.com>
2018-10-12 19:13:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,16 @@ class Artist
 	    @@all #displays the all array
 	  end
 	  
-	  
+	  def self.find_or_create_by_name(artist_name)
+	    found_artist = self.all.find {|artist| artist.name == artist_name}
+	    if found_artist
+	      found_artist
+	    else
+	      new_artist = self.new(artist_name)
+	      new_artist.save
+	      new_artist
+	    end
+	  end
 	  def save
 	    @@all << self #saves the artist name to the all array
 	  end
